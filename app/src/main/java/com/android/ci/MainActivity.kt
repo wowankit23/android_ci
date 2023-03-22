ec7a82b90508ed06cc6e5c98baa1789650baf24e
Ankit_Chhabra <ankit.chhabra@bjshomedelivery.com>
2023-03-22 14:30:16 +0530

    latest code

--- app/src/main/java/com/android/ci/MainActivity.kt
+++ app/src/main/java/com/android/ci/MainActivity.kt
@@ -2,10 +2,12 @@ package com.android.ci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this@MainActivity,"test",Toast.LENGTH_LONG).show()
     }
 }
